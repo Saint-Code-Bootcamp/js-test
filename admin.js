9037cf3c6711eb17b6d958333a24c494cf5281b4
Rurick <rurick80@gmail.com>
2020-07-18 10:26:41 +0400

    edit template

--- admin.js
+++ admin.js
@@ -4,14 +4,14 @@ const settings = require('./settings');
 
 function admin(req, res) { // результаты всех тестов
     const data_dir = settings.dirs.BASE + 'data/';
-    let list = {rows: []};
+    let list = [];
     const dirs = fs.readdirSync(data_dir);
     for (let i in dirs){
         const fn = dirs[i];
         const data = JSON.parse( fs.readFileSync(data_dir + fn) );
-        list.rows.push({email: data.email,
-                        summary: data.summary,
-                        complete: data.complete});
+        list.push({ email: data.email,
+                    summary: data.summary,
+                    complete: data.complete});
     }
     const compiledFunction = pug.compileFile(settings.dirs.TEMPLATES + 'admin.pug');
     const resp = compiledFunction({list: list});
